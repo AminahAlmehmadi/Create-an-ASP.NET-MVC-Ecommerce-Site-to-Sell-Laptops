33d4c7e5c8b2e83f033d1a591a5437fd7aad2129
AminahAlmehmadi <85236144+AminahAlmehmadi@users.noreply.github.com>
2021-09-01 04:51:40 +0300

    add project

---
+++ WebProject/dbo.product_1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[product] (
+    [ID]      INT            IDENTITY (1, 1) NOT NULL,
+    [Name]    NVARCHAR (MAX) NULL,
+    [Price]   FLOAT (53)     NOT NULL,
+    [Details] NVARCHAR (MAX) NULL,
+    [Image]   IMAGE (MAX) NULL,
+    [Count]   INT            NOT NULL,
+    CONSTRAINT [PK_product] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
